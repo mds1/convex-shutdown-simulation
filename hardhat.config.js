fe896748128223a1d0c92d3e1c2ab9d2740b38d8
Matt Solomon <matt@mattsolomon.dev>
2021-12-02 12:06:55 -0800

    fix: skip gas estimation for hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,6 +10,7 @@ module.exports = {
     hardhat: {
       accounts: { mnemonic: 'test test test test test test test test test test test junk' },
       chainId: 31337,
+      gas: 30_000_000, // set to block gas limit, which avoids calls to eth_estimateGas (results in same behavior as dapptools/foundry)
       forking: { url: process.env.ETH_RPC_URL, blockNumber: 13724056 },
     },
   },
