aa2e54ef3dd60011c72aed9a67e67b2435cf2cd0
Matt Solomon <matt@mattsolomon.dev>
2022-01-20 10:56:48 -0800

    dapptools: emit logs for easy success verification

--- scripts/benchmark-dapptools.sh
+++ scripts/benchmark-dapptools.sh
@@ -2,4 +2,4 @@
 
 # dapptools has no option to set gas limit, but it's hardcoded by default
 # anyway so shouldn't be make a difference
-time dapp test --rpc-url $ETH_RPC_URL --rpc-block $FORK_BLOCK
+time dapp test --verbosity 2 --rpc-url $ETH_RPC_URL --rpc-block $FORK_BLOCK
