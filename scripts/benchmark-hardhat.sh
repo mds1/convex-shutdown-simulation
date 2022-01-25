0cb655ee320541d73528229d3670df33b2f5fc72
Matt Solomon <matt@mattsolomon.dev>
2022-01-25 05:39:47 -0800

    chore: add comments

--- scripts/benchmark-hardhat.sh
+++ scripts/benchmark-hardhat.sh
@@ -4,4 +4,7 @@ if [ $CLEAR_CACHE = 1 ]; then
   yarn hardhat clean
 fi
 
+# When running Hardhat scripts without the CLI, you need to use ts-node's 
+# --files flag. This can also be enabled with TS_NODE_FILES=true.
+# https://hardhat.org/guides/typescript.html#running-your-tests-and-scripts-directly-with-ts-node
 time TS_NODE_FILES=true yarn ts-node ./scripts/convex.hardhat.ts
