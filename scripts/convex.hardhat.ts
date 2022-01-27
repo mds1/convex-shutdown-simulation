ac8a60a5af46bf5e12e43768b966fc44c7edbd78
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2022-01-27 12:52:58 -0600

    explicitly apply GAS_LIMIT to shutdownSystem()
    "If you are using ethers.js, then those values need to be set in each
    call/deployment.
    
    "The gasPrice that can be set in the config doesn't have an effect when
    you are using ethers. We need to update that section of the docs to make
    it clearer that that's the case."
    
    source: https://twitter.com/fvictorio_nan/status/1470503282909093890
    
    I confirmed with fvictorio that the same advice applies to the gas limit
    as well.
    
    I confirmed that, before this change, the log from the hardhat benchmark
    run showed a call to eth_estimateGas, and after this change it did not.

--- scripts/convex.hardhat.ts
+++ scripts/convex.hardhat.ts
@@ -27,7 +27,9 @@ async function main(): Promise<void> {
   console.log(chalk.bold('Simulating shutdown...'));
   console.time('simulate-shutdown');
   console.group();
-  const tx = await convex.connect(owner).shutdownSystem();
+  const tx = await convex.connect(owner).shutdownSystem(
+    { gasLimit: process.env.GAS_LIMIT }
+  );
   const receipt = await ethers.provider.getTransactionReceipt(tx.hash);
   assert.ok(receipt.status, `transaction failed. receipt: ${JSON.stringify(receipt)}`);
   console.groupEnd();
