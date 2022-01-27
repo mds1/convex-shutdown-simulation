efd5dc40e2ac82597be3e70b7b09d6507cccf1d4
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2022-01-26 18:20:04 -0600

    measure separate setup/execution like ganache

--- scripts/convex.hardhat.ts
+++ scripts/convex.hardhat.ts
@@ -1,9 +1,14 @@
 import * as assert from 'assert';
 
+const chalk: any = require('chalk');
 import { ethers, network } from 'hardhat';
 import '@nomiclabs/hardhat-ethers';
 
 async function main(): Promise<void> {
+  console.log(chalk.bold('Setting up Hardhat...'));
+  console.time('setup-hardhat');
+  console.group();
+
   // Impersonate owner
   const abi = ['function shutdownSystem() external', 'function owner() external view returns (address)'];
   const convex = new ethers.Contract('0xF403C135812408BFbE8713b5A23a04b3D48AAE31', abi, ethers.provider);
@@ -14,10 +19,20 @@ async function main(): Promise<void> {
   // Fund owner (it's a contract)
   await network.provider.send('hardhat_setBalance', [owner.address, '0xffffffffffffffffffff']);
 
+  console.groupEnd();
+  console.timeEnd('setup-hardhat');
+  console.log('');
+
   // Execute transaction
+  console.log(chalk.bold('Simulating shutdown...'));
+  console.time('simulate-shutdown');
+  console.group();
   const tx = await convex.connect(owner).shutdownSystem();
   const receipt = await ethers.provider.getTransactionReceipt(tx.hash);
   assert.ok(receipt.status, `transaction failed. receipt: ${JSON.stringify(receipt)}`);
+  console.groupEnd();
+  console.timeEnd('simulate-shutdown');
+  console.log('');
 }
 
 main()
