43459c5dbeb9276e1b208ee13f09a6cabf2a44f4
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2022-02-01 10:17:31 -0600

    testDockerfile: use EXAMPLE_CMD_PREFIX in sed too

--- testDockerfile.sh
+++ testDockerfile.sh
@@ -8,7 +8,10 @@ EXAMPLE_CMD_PREFIX="^#   "
 
 # Extract all of the example commands from the Dockerfile, in the order they
 # appear:
-EXAMPLE_CMDS=$(grep -e $EXAMPLE_CMD_PREFIX Dockerfile | sed -e "s/^#   //")
+EXAMPLE_CMDS=$( \
+    grep -e $EXAMPLE_CMD_PREFIX Dockerfile \
+        | sed -e "s/${EXAMPLE_CMD_PREFIX}//" \
+)
 
 # Run all of the example commands:
 IFS=$'\n'; for i in $EXAMPLE_CMDS; do
