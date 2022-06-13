d22070d5b177895466ab4a86446f8df401581b12
MicaiahReid <micaiahreid@gmail.com>
2022-06-13 10:23:59 -0400

    explicitly set ganache provider options type

--- scripts/convex.ganache.ts
+++ scripts/convex.ganache.ts
@@ -92,7 +92,7 @@ async function prepareGanache({
   ganache: ReturnType<typeof Ganache.provider>;
   provider: ethers.providers.JsonRpcProvider;
 }> {
-  const ganache = Ganache.provider({
+  const options: ProviderOptions = {
     fork: {
       url,
       blockNumber,
@@ -108,8 +108,10 @@ async function prepareGanache({
     logging: {
       quiet: false,
     }
-  });
 
+  const ganache = Ganache.provider(options);
+
+  // @ts-ignore currently ethers doesn't like ganache's provider type
   const provider = new ethers.providers.Web3Provider(ganache);
 
   return { ganache, provider };
